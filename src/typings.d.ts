7879c2e6b076c3f1b9482cd2b0c86b8a12e173c1
morolty85 <morolty85@gmail.com>
2019-09-23 16:49:27 +0900

    package added: redux develop tools / image files ext. declared

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,8 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export = content;
 }
+
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.bmp';
